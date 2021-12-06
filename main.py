2fc86a0335250daa5fccbea8128a054abbb26cbd
ivan <lucky.boy.fot@gmail.com>
2021-12-06 21:07:53 +0200

    make it better

--- main.py
+++ main.py
@@ -18,3 +18,21 @@ class Money:
     def to_string(self):
         value = float(str(self.__long) + "." + str(self.__byte))
         return value
+
+    def read(self):
+        self.set_long(input("Введіть кількість гривень: "))
+        self.set_byte(input("введіть кількість копійок: "))
+
+    def sub(self, other):
+        print(self.to_string() - other)
+
+    def mul_on_float(self):
+        print(self.to_string() * 1.5)
+
+    def compere(self, other):
+        if self.to_string() > other:
+            print("перше значення більше")
+        elif self.to_string() < other:
+            print("друге число більше")
+        else:
+            print("вони рівні")
--- worker.py
+++ worker.py
@@ -1,47 +1,16 @@
 from main import Money
 
 
-def read():
-    long = input("Введіть першу кількість гривень: ")
-    byte = input("введіть першу кількість копійок: ")
-    money = Money()
-    money.set_long(long)
-    money.set_byte(byte)
-    long1 = input("Введіть  другу кількість гривень: ")
-    byte1 = input("введіть другу кількість копійок: ")
-    money1 = Money()
-    money1.set_long(long1)
-    money1.set_byte(byte1)
-    long2 = input("Введіть  третю кількість гривень: ")
-    byte2 = input("введіть третю кількість копійок: ")
-    money2 = Money()
-    money2.set_long(long2)
-    money2.set_byte(byte2)
-    long3 = input("Введіть  третю кількість гривень: ")
-    byte3 = input("введіть третю кількість копійок: ")
-    money3 = Money()
-    money3.set_long(long3)
-    money3.set_byte(byte3)
-    float_number = float(input("Введіть дробове число"))
-    return [money.to_string(), money1.to_string(), money2.to_string(), money3.to_string(), float_number]
-
-
 def main_func():
-    get_read = read()
-    first_sum = get_read[0] + get_read[1]
-    second_sum = get_read[2] + get_read[3]
-    sub_of_sum = first_sum - second_sum
-    print("Різниця сум: ", sub_of_sum)
-    mul_on_float = get_read[0] * get_read[4]
-    print("Множення першого значення на дріб:", mul_on_float)
-    if first_sum > second_sum:
-        print("Перша сума більша")
-    elif first_sum < second_sum:
-        print("Друга сума більша")
-    else:
-        print("Суми однакові")
+    first_value = Money()
+    first_value.read()
+    second_value = Money()
+    second_value.read()
+    first_value.to_string()
+    second_value.to_string()
+    first_value.sub(second_value.to_string())
+    first_value.mul_on_float()
+    first_value.compere(second_value.to_string())
 
-def display():
-    main_func()
 
-display()
+main_func()
